debb5fe01b7674ae0a8af4e897b303bf0f389f7d
Asaf Abuzarli <asef.abuzerli.dev@gmail.com>
2023-05-08 21:07:13 +0400

    Loop instruction

--- Loop Instruction/Program.cs
+++ Loop Instruction/Program.cs
@@ -7,128 +7,130 @@ namespace Loop_Instruction
         static void Main(string[] args)
         {
 
-            //1-den 10 a qeder ededler
-            //1-den 10 a qeder ededler (10 da daxil)
-            //int i = 0;
+        //1-den 10 a qeder ededler
+        //1-den 10 a qeder ededler (10 da daxil)
+        //int i = 0;
 
-            //for (i = 0; i <= 10;)
-            //{
-            //    if (i % 2 == 0)
-            //    {
-            //        Console.WriteLine(i);
-            //    }
-            //    i++;
-            //}
+        //for (i = 0; i <= 10;)
+        //{
+        //    if (i % 2 == 0)
+        //    {
+        //        Console.WriteLine(i);
+        //    }
+        //    i++;
+        //}
 
-            //for (int i = 0; i < 10; i++)
-            //    Console.WriteLine(i);
+        //for (int i = 0; i < 10; i++)
+        //    Console.WriteLine(i);
 
-            //for (int i = 0;i < 10; i++) 
-            //    Console.WriteLine(i);
+        //for (int i = 0;i < 10; i++) 
+        //    Console.WriteLine(i);
 
-            //for (int i = 0; i < 10; i++)
-            //{
-            //    for (int j = 0; j < i; j++)
-            //    {
-            //        Console.Write("*");
-            //    }
-            //    Console.WriteLine();
-            //}
-
-
-            //for (int i = 10, j = 2; i >= j; i--)
-            //{
-            //    Console.WriteLine(i);
-            //}
+        //for (int i = 0; i < 10; i++)
+        //{
+        //    for (int j = 0; j < i; j++)
+        //    {
+        //        Console.Write("*");
+        //    }
+        //    Console.WriteLine();
+        //}
 
 
-            //for (int i = 0; i < 10; i++)
-            //{
-            //    for (int j = 9; j >= 0; j--)
-            //    {
-            //        Console.Write("*");
-            //    }
-            //    Console.WriteLine();
-            //}
-
-            //int a = 10;
-            //bool iteration = true;
-
-            //while (a > 50 && a < 100)
-            //{
-            //    if (a <= 0)
-            //        iteration = !iteration;
-
-            //    Console.WriteLine(a);
-            //    a--;
-            //}
-
-            //while (true)
-            //    Console.WriteLine("test");
-
-            //while (true) ;
-
-            //int a = 0;
-            //bool i = true;
-
-            //do
-            //{
-            //    if (a > 10)
-            //        i = !i;
-
-            //    Console.WriteLine("test " + a);
-            //    a++;
-            //}
-            //while (false);
-
-            //do
-            //    Console.WriteLine("test " + a);
-            //while (false);
-
-            //do { }
-            //while (true);
+        //for (int i = 10, j = 2; i >= j; i--)
+        //{
+        //    Console.WriteLine(i);
+        //}
 
 
-            //foreach (int x in Enumerable.Range(0,100))
-            //{
-            //    Console.WriteLine(x);
-            //}
+        //for (int i = 0; i < 10; i++)
+        //{
+        //    for (int j = 9; j >= 0; j--)
+        //    {
+        //        Console.Write("*");
+        //    }
+        //    Console.WriteLine();
+        //}
 
-            b:
+        //int a = 10;
+        //bool iteration = true;
 
-            //break -  
-            //continue - 
-            //goto
-            //return
+        //while (a > 50 && a < 100)
+        //{
+        //    if (a <= 0)
+        //        iteration = !iteration;
 
-            //for (int i = 1; i < 10; i++)
-            //{
-            //    for (int j = 1; j < 10; j++)
-            //    {
-            //        if (j % 9 == 0)
-            //            break;
-            //        Console.WriteLine(j);
-            //    }
-            //}
+        //    Console.WriteLine(a);
+        //    a--;
+        //}
 
-            //for (int i = 0; i < 100; i++)
-            //{
-            //    if (i % 10 == 0)
-            //    {
-            //        continue;
-            //    }
-            //    Console.WriteLine(i);
-            //}
+        //while (true)
+        //    Console.WriteLine("test");
+
+        //while (true) ;
+
+        //int a = 0;
+        //bool i = true;
+
+        //do
+        //{
+        //    if (a > 10)
+        //        i = !i;
+
+        //    Console.WriteLine("test " + a);
+        //    a++;
+        //}
+        //while (false);
+
+        //do
+        //    Console.WriteLine("test " + a);
+        //while (false);
+
+        //do { }
+        //while (true);
 
 
-            for (int i = 1; i < 100; i++)
-            {
-                if (i % 10 == 0)
-                {
-                    Console.WriteLine(i);
-                    goto b;
-                }
-            }
+        //foreach (int x in Enumerable.Range(0,100))
+        //{
+        //    Console.WriteLine(x);
+        //}
+
+
+
+        //break -  
+        //continue - 
+        //goto - 
+        //return
+
+        //for (int i = 1; i < 10; i++)
+        //{
+        //    for (int j = 1; j < 10; j++)
+        //    {
+        //        if (j % 9 == 0)
+        //            break;
+        //        Console.WriteLine(j);
+        //    }
+        //}
+
+        //for (int i = 0; i < 100; i++)
+        //{
+        //    if (i % 10 == 0)
+        //    {
+        //        continue;
+        //    }
+        //    Console.WriteLine(i);
+        //}
+
+        //b:
+
+        //    for (int i = 1; i < 100; i++)
+        //    {
+        //        a:
+        //        if (i % 10 == 0)
+        //        {
+        //            Console.WriteLine(i);
+        //            goto b;
+        //        }
+        //    }
 
         }
     }
