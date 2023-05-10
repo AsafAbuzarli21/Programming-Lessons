1e5b309b244b202c97fcef7ab84547d4ee604882
Asaf Abuzarli <aaabuzarli@xalqbank.az>
2023-05-10 15:22:15 +0400

    Useles codes deleted

--- Loop Instruction/Program.cs
+++ Loop Instruction/Program.cs
@@ -279,15 +279,6 @@
             //}
 
             #endregion
-
-            for (int i = 1; i <= 10; i++)
-            {
-                for (int j = 1; j <= 10; j++)
-                {
-                    Console.Write("{0}*{1}={2}\t", i, j, (i * j));
-                }
-                Console.WriteLine();
-            }
         }
     }
 }
